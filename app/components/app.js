f245de2c6cf73d9600a41e29975f6d35116a0f11
slhstudio <slhstudio@gmail.com>
2018-07-13 13:54:16 -0700

    first commit

---
+++ app/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        I found this type and I really like it!
+      </div>
+    )
+  }
+}
+
+export default App
