2c110cf800d903dfbc624aa7b21e93e2db680c38
slhstudio <slhstudio@gmail.com>
2018-10-12 22:10:54 -0700

    add credentials

--- app/services/passport-setup.js
+++ app/services/passport-setup.js
@@ -1,9 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+require('dotenv').config({ path: 'variables.env' });
 
 passport.use(
   new GoogleStrategy({
     //options for the google strategy
+    clientID: `${CLIENT_ID}`,
+    clientSecret: `${CLIENT_SECRET}`
   }), () => {
     //passport callback function
   }
